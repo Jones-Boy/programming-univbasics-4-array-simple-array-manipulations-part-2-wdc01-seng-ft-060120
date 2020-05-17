055fa15b345f391367169cf511923a43ec6f704c
Michael Jones <djyoungjones@gmail.com>
2020-05-17 15:23:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,7 +22,7 @@ end
 
 def using_delete(array, string)
   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-  no_offense_steven = instructors.delete "Steven"
+  no_offense_steven = instructors.delete("Steven")
 end 
 
 def using_delete_at(array, integer)
